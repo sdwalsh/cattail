5a1da941b147082c2dd343169e8bf652d48ef614
Sean Walsh <sdwalsh@mirango.io>
2017-01-17 04:15:58 -0500

    add convergence

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
 	"image"
 	_ "image/png"
 	"log"
@@ -32,18 +34,16 @@ type Centroid struct {
 	RGBPoint
 }
 
+// Given a color pointer update cluster with given centroid
 func setCentroid(color *Color, centroid Centroid) {
 	color.Cluster = centroid
 }
 
+// multiply two points and return component values
 func multiply(c1 Point, c2 Point) (uint32, uint32, uint32) {
 	r1, g1, b1 := c1.GetCoords()
 	r2, g2, b2 := c2.GetCoords()
-	r := r1 * r2
-	g := g1 * g2
-	b := b1 * b2
-
-	return r, g, b
+	return r1 * r2, g1 * g2, b1 * b2
 }
 
 // Find distance between two points
@@ -88,7 +88,6 @@ func generateCentroids(n int32) []Centroid {
 func addColors(m image.Image, centroids []Centroid) []Color {
 	bounds := m.Bounds()
 	var colors []Color
-	i := 0
 	for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
 		for x := bounds.Min.X; x < bounds.Max.X; x++ {
 			r, g, b, _ := m.At(x, y).RGBA()
@@ -111,9 +110,11 @@ func addColors(m image.Image, centroids []Centroid) []Color {
 	return colors
 }
 
-func filter(vs *[]Color, cs Centroid, f func(Color, Centroid) bool) []Color {
+// Given a slice of colors, a centroid, and a function to compareCentroid
+// a color and a centroid return a slice of colors that satisfy the function
+func filter(vs []Color, cs Centroid, f func(Color, Centroid) bool) []Color {
 	vsf := make([]Color, 0)
-	for _, v := range *vs {
+	for _, v := range vs {
 		if f(v, cs) {
 			vsf = append(vsf, v)
 		}
@@ -121,6 +122,7 @@ func filter(vs *[]Color, cs Centroid, f func(Color, Centroid) bool) []Color {
 	return vsf
 }
 
+// Compare the values of a color's cluster to a given centroid
 func compareCentroid(color Color, centroid Centroid) bool {
 	if color.Cluster == centroid {
 		return true
@@ -128,10 +130,12 @@ func compareCentroid(color Color, centroid Centroid) bool {
 	return false
 }
 
-func calcPosition(colors []Color, centroid *Centroid) *Centroid {
+// Given a slice of (filtered) colors and a pointer to a centroid recalc and update
+// the location of the centroid
+func calcPosition(colors []Color, centroid *Centroid) {
 	var rt, gt, bt uint32
 	total := uint32(len(colors))
-	for i, color := range colors {
+	for _, color := range colors {
 		r, g, b := color.GetCoords()
 		rt += r
 		gt += g
@@ -140,19 +144,20 @@ func calcPosition(colors []Color, centroid *Centroid) *Centroid {
 	centroid.Red = rt / total
 	centroid.Green = gt / total
 	centroid.Blue = bt / total
-
-	return centroid
 }
 
-func recalculateCentroids(colors *[]Color, centroids *[]Centroid) ([]Color, []Centroid) {
+func recalculateCentroids(colors *[]Color, centroids *[]Centroid) []Centroid {
+	var oldCentroids []Centroid
+	copy(oldCentroids, *centroids)
 	for _, centroid := range *centroids {
-		centroidColors := filter(colors, centroid, compareCentroid)
-		calcPosition(*colors, &centroid)
+		centroidColors := filter(*colors, centroid, compareCentroid)
+		calcPosition(centroidColors, &centroid)
 	}
 	for _, color := range *colors {
 		colorCentroid := nearestCentroid(color, *centroids)
 		setCentroid(&color, colorCentroid)
 	}
+	return oldCentroids
 }
 
 // Given an image location, open image and return an image.Image
@@ -167,19 +172,49 @@ func importImage(i string) image.Image {
 	if err != nil {
 		log.Fatal(err)
 	}
-
 	return m
+}
+
+func convergence(centroids []Centroid, oldCentroids []Centroid) bool {
+	if len(oldCentroids) == 0 {
+		return false
+	}
+	//var distances []float64
+	for _, centroid := range centroids {
+		for _, oldCentroid := range oldCentroids {
+			//append(distances, distance(centroid, oldCentroid))
+			if distance(centroid, oldCentroid) > 1.0 {
+				return false
+			}
+		}
+	}
+	return true
+
 }
 
 // Given an image and a number of desired clusters generate a slice
 // of colors
-func convertImage(m image.Image, n int32) []Color {
+func convertImage(m image.Image, n int32) ([]Color, []Centroid) {
 	centroids := generateCentroids(n)
-	return addColors(m, centroids)
+	return addColors(m, centroids), centroids
 }
 
 func main() {
-	m := importImage("testimage.png")
-	colors := convertImage(m, 6)
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("enter image name: ")
+	name, err := reader.ReadString('\n')
+	if err != nil {
+		log.Fatal("Error parsing name")
+	}
+	m := importImage(name)
+	colors, centroids := convertImage(m, 6)
+	var oldCentroids []Centroid
+	i := 0
+
+	for convergence(centroids, oldCentroids) || i < 20 {
+		oldCentroids = recalculateCentroids(&colors, &centroids)
+		i++
+	}
+	fmt.Printf("%#v", centroids)
 
 }
