ca479a8f02ca6e62e14bdfaa2e976aa7c6307e82
Sean Walsh <sdwalsh@mirango.io>
2017-01-17 23:21:01 -0500

    rework of types

--- main.go
+++ main.go
@@ -1,163 +1,26 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"image"
-	_ "image/png"
+	"image/draw"
+	_ "image/jpeg"
+	"image/png"
 	"log"
-	"math"
 	"math/rand"
 	"os"
+	"time"
+
+	"github.com/lucasb-eyer/go-colorful"
 )
 
-type Point interface {
-	GetCoords() (uint32, uint32, uint32)
-}
-
-type RGBPoint struct {
-	Red   uint32
-	Green uint32
-	Blue  uint32
-}
-
-func (rgb RGBPoint) GetCoords() (uint32, uint32, uint32) {
-	return rgb.Red, rgb.Green, rgb.Blue
-}
-
 type Color struct {
-	RGBPoint
-	Cluster Centroid
+	Color   colorful.Color
+	Cluster *Centroid
 }
 
 type Centroid struct {
-	RGBPoint
-}
-
-// Given a color pointer update cluster with given centroid
-func setCentroid(color *Color, centroid Centroid) {
-	color.Cluster = centroid
-}
-
-// multiply two points and return component values
-func multiply(c1 Point, c2 Point) (uint32, uint32, uint32) {
-	r1, g1, b1 := c1.GetCoords()
-	r2, g2, b2 := c2.GetCoords()
-	return r1 * r2, g1 * g2, b1 * b2
-}
-
-// Find distance between two points
-func distance(c1 Point, c2 Point) float64 {
-	r1, g1, b1 := c1.GetCoords()
-	r2, g2, b2 := c2.GetCoords()
-	r := r1 - r2
-	g := g1 - g2
-	b := b1 - b2
-	return math.Sqrt(float64(r*r + g*g + b*b))
-}
-
-// Iterate through centroids to deterimine which is closest
-// O(n) running time
-func nearestCentroid(c1 Point, centroids []Centroid) Centroid {
-	lowestDistance := float64(0.0)
-	index := -1
-	for i, centroid := range centroids {
-		distance := distance(c1, centroid)
-		if distance <= lowestDistance {
-			lowestDistance = distance
-			index = i
-		}
-	}
-	return centroids[index]
-}
-
-// Generate n number of centroids
-func generateCentroids(n int32) []Centroid {
-	centroids := make([]Centroid, n)
-	for i, _ := range centroids {
-		r := uint32(rand.Intn(65535))
-		g := uint32(rand.Intn(65535))
-		b := uint32(rand.Intn(65535))
-		centroids[i] = Centroid{RGBPoint{Red: r, Green: g, Blue: b}}
-	}
-	return centroids
-}
-
-// Takes an image and a list of pregenerated centroids to generate
-// a list of colors - points with an assigned cluster
-func addColors(m image.Image, centroids []Centroid) []Color {
-	bounds := m.Bounds()
-	var colors []Color
-	for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
-		for x := bounds.Min.X; x < bounds.Max.X; x++ {
-			r, g, b, _ := m.At(x, y).RGBA()
-			cluster := nearestCentroid(RGBPoint{
-				Red:   r,
-				Green: g,
-				Blue:  b,
-			},
-				centroids)
-			colors = append(colors, Color{
-				RGBPoint: RGBPoint{
-					Red:   r,
-					Green: g,
-					Blue:  b,
-				},
-				Cluster: cluster,
-			})
-		}
-	}
-	return colors
-}
-
-// Given a slice of colors, a centroid, and a function to compareCentroid
-// a color and a centroid return a slice of colors that satisfy the function
-func filter(vs []Color, cs Centroid, f func(Color, Centroid) bool) []Color {
-	vsf := make([]Color, 0)
-	for _, v := range vs {
-		if f(v, cs) {
-			vsf = append(vsf, v)
-		}
-	}
-	return vsf
-}
-
-// Compare the values of a color's cluster to a given centroid
-func compareCentroid(color Color, centroid Centroid) bool {
-	if color.Cluster == centroid {
-		return true
-	}
-	return false
-}
-
-// Given a slice of (filtered) colors and a pointer to a centroid recalc and update
-// the location of the centroid
-func calcPosition(colors []Color, centroid *Centroid) {
-	var rt, gt, bt uint32
-	total := uint32(len(colors))
-	for _, color := range colors {
-		r, g, b := color.GetCoords()
-		rt += r
-		gt += g
-		bt += b
-	}
-	centroid.Red = rt / total
-	centroid.Green = gt / total
-	centroid.Blue = bt / total
-}
-
-func recalculateCentroids(colors *[]Color, centroids *[]Centroid) []Centroid {
-	var oldCentroids []Centroid
-	copy(oldCentroids, *centroids)
-	for _, centroid := range *centroids {
-		centroidColors := filter(*colors, centroid, compareCentroid)
-		calcPosition(centroidColors, &centroid)
-	}
-	for _, color := range *colors {
-		colorCentroid := nearestCentroid(color, *centroids)
-		setCentroid(&color, colorCentroid)
-	}
-	return oldCentroids
+	Color colorful.Color
 }
 
 // Given an image location, open image and return an image.Image
@@ -175,46 +38,239 @@ func importImage(i string) image.Image {
 	return m
 }
 
-func convergence(centroids []Centroid, oldCentroids []Centroid) bool {
-	if len(oldCentroids) == 0 {
-		return false
-	}
-	//var distances []float64
+// Iterate through centroids to deterimine which is closest using CIE94
+// CIE94 provides distance method that is closer to human perception
+// Delta E	Perception
+// <= 1.0	Not perceptible by human eyes.
+// 1 - 2	Perceptible through close observation.
+// 2 - 10	Perceptible at a glance.
+// 11 - 49	Colors are more similar than opposite
+// 100	Colors are exact opposite
+// http://zschuessler.github.io/DeltaE/learn/
+func nearestCentroid(c1 colorful.Color, centroids []*Centroid) *Centroid {
+	// set lowest distance to max value (opposites)
+	lowestDistance := 100.0
+	var address *Centroid
 	for _, centroid := range centroids {
-		for _, oldCentroid := range oldCentroids {
-			//append(distances, distance(centroid, oldCentroid))
-			if distance(centroid, oldCentroid) > 1.0 {
-				return false
-			}
+		distance := c1.DistanceCIE94(centroid.Color)
+		if distance < lowestDistance {
+			lowestDistance = distance
+			address = centroid
 		}
 	}
-	return true
-
+	return address
 }
 
-// Given an image and a number of desired clusters generate a slice
-// of colors
-func convertImage(m image.Image, n int32) ([]Color, []Centroid) {
+// Takes an image and a list of pregenerated centroids to generate
+// a list of colors (points with an assigned cluster)
+// bounds do not necessarily start at 0, use bounds.Min and bounds.Max instead
+// colorful.Color takes float64 [0..1] divide by alpha pre-multiplied value provided by RGBA()
+func addColors(m image.Image, centroids []*Centroid) []Color {
+	bounds := m.Bounds()
+	var colors []Color
+	for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
+		for x := bounds.Min.X; x < bounds.Max.X; x++ {
+			r, g, b, _ := m.At(x, y).RGBA()
+			color := colorful.Color{R: float64(r) / float64(65535), G: float64(g) / float64(65535), B: float64(b) / float64(65535)}
+			cluster := nearestCentroid(color, centroids)
+			colors = append(colors, Color{Color: color, Cluster: cluster})
+		}
+	}
+	return colors
+}
+
+// Generate n number of centroids between 0 - 1
+func generateCentroids(n int32) []*Centroid {
+	centroids := make([]*Centroid, 0)
+	for i := int32(0); i < n; i++ {
+		r := rand.Float64()
+		g := rand.Float64()
+		b := rand.Float64()
+		centroids = append(centroids, &Centroid{Color: colorful.Color{R: r, G: g, B: b}})
+	}
+	if len(centroids) != 0 {
+		return centroids
+	} else {
+		log.Fatal("centroids incorrectly generated")
+		return centroids
+	}
+}
+
+// Given an image and a number of desired clusters generate a slice of colors and
+// a slice of centroid pointers
+func convertImage(m image.Image, n int32) ([]Color, []*Centroid) {
 	centroids := generateCentroids(n)
 	return addColors(m, centroids), centroids
 }
 
-func main() {
-	reader := bufio.NewReader(os.Stdin)
-	fmt.Print("enter image name: ")
-	name, err := reader.ReadString('\n')
-	if err != nil {
-		log.Fatal("Error parsing name")
+// Given a slice of colors, a centroid, and a function to compareCentroid
+// a color and a centroid return a slice of colors that satisfy the function
+func filter(vs []Color, cs *Centroid, f func(Color, *Centroid) bool) []Color {
+	vsf := make([]Color, 0)
+	for _, v := range vs {
+		if f(v, cs) {
+			vsf = append(vsf, v)
+		}
 	}
-	m := importImage(name)
-	colors, centroids := convertImage(m, 6)
-	var oldCentroids []Centroid
-	i := 0
+	return vsf
+}
 
-	for convergence(centroids, oldCentroids) || i < 20 {
-		oldCentroids = recalculateCentroids(&colors, &centroids)
+// Compare the values of a color's cluster to a given centroid
+func compareCentroid(color Color, centroid *Centroid) bool {
+	if *color.Cluster == *centroid {
+		return true
+	}
+	return false
+}
+
+// Recalculate the centroids using average of l, a, b components of the colors
+func recalculateCentroids(colors []Color, centroids []*Centroid) {
+	for _, centroid := range centroids {
+		var lt, at, bt float64
+		centroidColors := filter(colors, centroid, compareCentroid)
+		//fmt.Printf("filtered colors:: %v \n", len(centroidColors))
+		total := float64(len(centroidColors))
+		if total == 0.0 {
+			fmt.Println("------------EMPTY-----------")
+		} else {
+			for _, color := range centroidColors {
+				l, a, b := color.Color.Lab()
+				lt += l
+				at += a
+				bt += b
+			}
+			if centroid.Color != colorful.Lab(lt/total, at/total, bt/total) {
+				fmt.Println("------------UPDATED-----------")
+				fmt.Printf("old:: %v \t", centroid.Color.Hex())
+				fmt.Printf("new:: %v \t", colorful.Lab(lt/total, at/total, bt/total).Hex())
+			}
+			centroid.Color = colorful.Lab(lt/total, at/total, bt/total)
+			fmt.Printf("current:: %v \n", centroid.Color.Hex())
+			fmt.Println("------------END-----------")
+			//fmt.Printf("%v \n", colorful.Lab(lt/total, at/total, bt/total).Hex())
+		}
+	}
+}
+
+// Alter colors in place (colors is a very, very large slice - copying to a new
+// slice would be costly)
+func recalculateColors(colors []Color, centroids []*Centroid) []Color {
+	var newColors []Color
+	for _, color := range colors {
+		cluster := nearestCentroid(color.Color, centroids)
+		newColors = append(newColors, Color{Color: color.Color, Cluster: cluster})
+	}
+	return newColors
+}
+
+func createColorImage(m image.Image, centroids []*Centroid) {
+	bounds := m.Bounds()
+	x_0 := 0
+	y_0 := 0
+	height := bounds.Max.Y - bounds.Min.Y
+	width := bounds.Max.X - bounds.Min.X
+
+	// new image that will serve as the generated picture (x0, y0, x1, y1)
+	img := image.NewRGBA(image.Rect(x_0, y_0, width, height))
+
+	for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
+		for x := bounds.Min.X; x < bounds.Max.X; x++ {
+			r, g, b, _ := m.At(x, y).RGBA()
+			color := colorful.Color{R: float64(r) / float64(65535), G: float64(g) / float64(65535), B: float64(b) / float64(65535)}
+			cluster := nearestCentroid(color, centroids)
+			draw.Draw(img, image.Rect(x_0, y_0, x_0+1, y_0+1), &image.Uniform{cluster.Color}, image.ZP, draw.Src)
+			x_0++
+		}
+		x_0 = 0
+		y_0++
+	}
+
+	toimg, err := os.Create("colorblend.png")
+	if err != nil {
+		fmt.Printf("Error: %v", err)
+		return
+	}
+	defer toimg.Close()
+
+	png.Encode(toimg, img)
+}
+
+func convergence(centroids []*Centroid, oldCentroids []Centroid) bool {
+	if len(oldCentroids) == 0 {
+		return false
+	}
+	for i, centroid := range centroids {
+		distance := oldCentroids[i].Color.DistanceCIE94(centroid.Color)
+		if distance > 2.0 {
+			return false
+		}
+	}
+	return true
+}
+
+// Reroll the random position of a centroid (useful if empty)
+func (c *Centroid) reroll(colors []Color) {
+
+}
+
+func main() {
+	//reader := bufio.NewReader(os.Stdin)
+	//fmt.Print("enter image name: ")
+	//name, err := reader.ReadString('\n')
+	//if err != nil {
+	//	log.Fatal("Error parsing name")
+	//}
+	rand.Seed(time.Now().UTC().UnixNano())
+	fmt.Println("begin timer")
+	startTime := time.Now()
+
+	m := importImage("./lexington.jpg")
+	fmt.Printf("image imported\n")
+
+	colors, centroids := convertImage(m, 16)
+	// detect and remove empty centroids
+	/*for _, centroid := range centroids {
+		// reroll centroid until it at least one color
+		for len(filter(colors, centroid, compareCentroid)) != 0 {
+		}
+	} */
+	fmt.Printf("image converted\n")
+	fmt.Printf("colors size :%v \n", len(colors))
+	fmt.Println("/////////////////////////////////////////////////")
+	for i, centroid := range centroids {
+		fmt.Printf("centroid #%v: ", i)
+		fmt.Printf("%v \n", len(filter(colors, centroid, compareCentroid)))
+	}
+	fmt.Println("/////////////////////////////////////////////////")
+	var oldCentroids []Centroid
+
+	i := 0
+	fmt.Printf("begin convergence tests\n")
+	fmt.Println("-------------------------------------------------")
+	for !convergence(centroids, oldCentroids) && i < 12 {
+		fmt.Printf("loop #%v\n", i)
+		t1 := time.Now()
+
+		for _, centroid := range centroids {
+			oldCentroids = append(oldCentroids, *centroid)
+		}
+
+		recalculateCentroids(colors, centroids)
+		colors = recalculateColors(colors, centroids)
+
+		t2 := time.Now()
+		fmt.Printf("time: %v \n", t2.Sub(t1))
+		for i, centroid := range centroids {
+			fmt.Printf("centroid #%v: ", i)
+			fmt.Printf("%v \t", len(filter(colors, centroid, compareCentroid)))
+			fmt.Printf("%v \n", centroid.Color.Hex())
+		}
+		fmt.Println("\n/////////////////////////////////////////////////")
 		i++
 	}
-	fmt.Printf("%#v", centroids)
-
+	fmt.Println("begin rendering")
+	createColorImage(m, centroids)
+	endTime := time.Now()
+	//fmt.Printf("%+v \n", centroids)
+	fmt.Printf("total time to complete: %v \n", endTime.Sub(startTime))
 }
